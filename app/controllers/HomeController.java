fe20b5c0a9200c8aaec4fec897109f457c1321bc
CEIBA\julian.rivera <julian.rivera@ceiba.com.co>
2018-07-16 16:18:31 -0500

    Retos de Api de configuración

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -2,8 +2,12 @@ package controllers;
 
 import java.util.Date;
 
-import com.google.gson.Gson;
+import javax.inject.Inject;
 
+import com.google.gson.Gson;
+import com.typesafe.config.Config;
+
+import play.Logger;
 import play.mvc.*;
 
 import views.html.*;
@@ -14,15 +18,24 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
+	private final Config config;
+
+	@Inject
+	public HomeController(Config config) {
+		this.config = config;
+	}
+
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public Result index() {
+		System.out.println(config.getString("application.name"));
+		Logger.warn("-------------- Prueba de uso del log --------------------");
+		return ok(index.render("Your new application is ready."));
+	}
     
     public Result helloWorld() {
         return ok("Hello world!");
